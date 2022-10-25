9edec1ad56a6630da8e43177d7fc220b819609f1
eliblurr <elvissegbawu@gmail.com>
2022-10-25 16:45:42 +0000

    add color

--- nsra/base/choices.py
+++ nsra/base/choices.py
@@ -1,7 +1,11 @@
 COLORS=[
         ('', 'Select a color'),
-        ('#EB3438', 'red'),
-        ('#F4E726', 'yellow'),
-        ('#009933', 'green'),
-        ('#232323', 'black'),
+        # ('#EB3438', 'red'),
+        # ('#F4E726', 'yellow'),
+        # ('#009933', 'green'),
+        # ('#232323', 'black'),
+        ('#EB3232', 'red'),
+        ('#FFCC00', 'yellow'),
+        ('#006633', 'green'),
+        ('#201E1E', 'black'),
     ]
